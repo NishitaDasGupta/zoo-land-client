a45e74aab454bc493b38c18751520e3f30682409
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-18 17:51:52 +0600

    firebase env done

--- src/Config/firebase.config.js
+++ src/Config/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBX6w-qBxlDlS0Ztfe5BlrLMvXVotpAS6k",
-  authDomain: "zoo-land-client.firebaseapp.com",
-  projectId: "zoo-land-client",
-  storageBucket: "zoo-land-client.appspot.com",
-  messagingSenderId: "772361923825",
-  appId: "1:772361923825:web:4a27067da3c382a0bbcd50"
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
