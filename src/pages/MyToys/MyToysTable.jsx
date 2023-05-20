9a4af3082766fdefd82861ac7fb4017d3c796b25
NishitaDasGupta <nishitadasguptamou@gmail.com>
2023-05-20 09:58:51 +0600

    my toy using tabular form

--- src/pages/MyToys/MyToysTable.jsx
+++ src/pages/MyToys/MyToysTable.jsx
@@ -5,7 +5,17 @@ const MyToysTable = ({toy,index}) => {
         const { _id, availableQuantity, price, seller, subcategory, toyName } = toy;
         return (
             <tr>
-                
+                <th>{index+1}</th>
+                <td>{seller}</td>
+                <td>{toyName}</td>
+                <td>{subcategory}</td>
+                <td>${price}</td>
+                <td className="text-center">{availableQuantity}</td>
+                <td>
+                    
+                    <button  className="btn bg-orange-400 hover:bg-orange-600">Details</button>
+                   
+                </td>
             </tr>
     );
 };
